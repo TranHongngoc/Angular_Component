59b69947e7b436d1aaaad42907185a6aedc42369
hongngoc <hongngoctran0@gmail.com>
2019-02-12 14:13:56 +0700

    add countdown-time

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 import { RatingBarComponent } from './rating-bar/rating-bar.component';
+import { CountdownTimerComponent } from './countdown-timer/countdown-timer.component';
+import { CountdownTimerGetSetComponent } from './countdown-timer-get-set/countdown-timer-get-set.component';
+import { CountdownTimerAliasComponent } from './countdown-timer-alias/countdown-timer-alias.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProgressBarComponent,
-    RatingBarComponent
+    RatingBarComponent,
+    CountdownTimerComponent,
+    CountdownTimerGetSetComponent,
+    CountdownTimerAliasComponent
   ],
   imports: [
     BrowserModule
